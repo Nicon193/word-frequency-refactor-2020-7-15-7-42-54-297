f07e08ed4e57ba825ebdbac9907941cc956e5e9c
Albert <Albert.c.x.chen@oocl.com>
2020-07-27 20:35:17 +0800

    [word-frequency][Albert] rename value to word , s to word ,i to count

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,14 +1,14 @@
 public class Input {
-    private String value;
+    private String word;
     private int count;
 
-    public Input(String w, int i) {
-        this.value = w;
-        this.count = i;
+    public Input(String word, int count) {
+        this.word = word;
+        this.count = count;
     }
 
     public String getValue() {
-        return this.value;
+        return this.word;
     }
 
     public int getWordCount() {
